f44edf9b2a9b8882bfc639913fe8946affe1b1ea
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2024-05-24 17:40:19 +0100

    refactoring of code and ui

--- app/component/StudentForm.tsx
+++ app/component/StudentForm.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm, SubmitHandler } from "react-hook-form";
+import Alert from "@mui/material/Alert";
 
 type Inputs = {
    example: string;
@@ -13,16 +14,14 @@ type StudentType = {
    studentNumber: string;
 };
 
-export default function StudentForm({closeTab}:any) {
-     const queryClient = useQueryClient();
+export default function StudentForm({ closeTab }: any) {
+   const queryClient = useQueryClient();
 
    const {
       register,
-      handleSubmit,     
+      handleSubmit,
       formState: { errors },
    } = useForm<StudentType>();
-
-
 
    const mutation = useMutation({
       mutationFn: (data: StudentType) => {
@@ -43,50 +42,92 @@ export default function StudentForm({closeTab}:any) {
          queryClient.invalidateQueries({ queryKey: ["studentData"] });
          closeTab();
       },
+      onError: () => {
+         closeTab();
+      },
    });
 
    const onSubmit: SubmitHandler<StudentType> = (data) => {
       mutation.mutate(data);
-
    };
-    
-    
 
    return (
-      <div className="p">
-         <form onSubmit={handleSubmit(onSubmit)}>
-            <label>nationalId</label>
-            <input
-               {...register("nationalId", { required: true })}
-               className="border"
-            />
-            {errors.nationalId && <span>This field is required</span>}
-            <label>name</label>
-            <input
-               {...register("name", { required: true })}
-               className="border"
-            />
-            {errors.name && <span>This field is required</span>}
-            <label>surname</label>
-            <input
-               {...register("surname", { required: true })}
-               className="border"
-            />
-            {errors.surname && <span>This field is required</span>}
-            <label>Date of birth</label>
-            <input
-               {...register("dateOfBirth", { required: true })}
-               className="border"
-            />
-            {errors.dateOfBirth && <span>This field is required</span>}
-            <label>studentNumber</label>
-            <input
-               {...register("studentNumber", { required: true })}
-               className="border"
-            />
-            {errors.studentNumber && <span>This field is required</span>}
+      <div className="px-1 w-[400px]">
+<p className="text-center font-bold my-7">Fill in your necessary Information.</p>
 
-            <button type="submit" className="border">
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-1 mt-4">
+               <label className="font-[500]">National Id</label>
+               <input
+                  {...register("nationalId", { required: true })}
+                  className="border h-10 rounded pl-4 focus:outline-none w-full"
+                  placeholder="National Id"
+               />
+               {errors.nationalId && (
+                  <span className="text-sm text-red-400 italic font-thin text-start">
+                     This field is required
+                  </span>
+               )}
+            </div>
+
+            <div className="flex flex-col gap-1 mt-4">
+               <label className="font-[500]">Name</label>
+               <input
+                  {...register("name", { required: true })}
+                  className="border h-10 rounded pl-4 focus:outline-none"
+                  placeholder="Name"
+               />
+               {errors.name && (
+                  <span className="text-sm text-red-400 italic font-thin text-start">
+                     This field is required
+                  </span>
+               )}
+            </div>
+
+            <div className="flex flex-col gap-1 mt-4">
+               <label className="font-[500]">Surname</label>
+               <input
+                  {...register("surname", { required: true })}
+                  className="border h-10 rounded pl-4 focus:outline-none"
+                  placeholder="Surname"
+               />
+               {errors.surname && (
+                  <span className="text-sm text-red-400 italic font-thin text-start">
+                     This field is required
+                  </span>
+               )}
+            </div>
+            <div className="flex flex-col gap-1 mt-4">
+               <label className="font-[500]">Date of birth</label>
+               <input
+                  {...register("dateOfBirth", { required: true })}
+                  className="border h-10 rounded pl-4 focus:outline-none"
+                  placeholder="Date of birth"
+               />
+               {errors.dateOfBirth && (
+                  <span className="text-sm text-red-400 italic font-thin text-start">
+                     This field is required
+                  </span>
+               )}
+            </div>
+            <div className="flex flex-col gap-1 mt-4">
+               <label className="font-[500]">Student Number</label>
+               <input
+                  {...register("studentNumber", { required: true })}
+                  className="border h-10 rounded pl-4 focus:outline-none"
+                  placeholder="Student Number"
+               />
+               {errors.studentNumber && (
+                  <span className="text-sm text-red-400 italic font-thin text-start">
+                     This field is required
+                  </span>
+               )}
+            </div>
+
+            <button
+               type="submit"
+               className="py-2.5 px-5 me-2 mb-2 w-fit text-sm font-medium text-white  bg-gray-500  rounded-lg border border-gray-200 hover:bg-gray-100  hover:text-black mx-auto mt-8 "
+            >
                Submit{" "}
             </button>
          </form>
